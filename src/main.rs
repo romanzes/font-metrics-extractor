79aa226d7f39229d2311b73351eb5b7933898f59
Roman Petrenko <roman@canva.com>
2021-04-22 15:47:40 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use skia_safe::{Typeface, Data, Font};
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let data = data_from_file_path(Path::new("EvolveSans-Bold.ttf"));
+    let typeface = Typeface::from_data(data, None).unwrap();
+    let font = Font::new(typeface, 1.0);
+    let (_, metrics) = font.metrics();
+
+    println!("metrics.ascent: {}", metrics.ascent);
+    println!("metrics.descent: {}", metrics.descent);
+    println!("{}", mozjpeg::DCTSIZE);
+}
+
+pub fn data_from_file_path(file_path: &Path) -> Data {
+    let mut file = File::open(file_path).unwrap();
+    let mut bytes = vec![];
+    file.read_to_end(&mut bytes).unwrap();
+    Data::new_copy(&bytes.as_slice())
+}
